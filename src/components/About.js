41a9325d6d4d750bef6ad5f47aa5467a3e9c68c6
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2024-03-10 13:49:21 +0530

    About.jsx modified

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,23 @@ import React from "react";
 
 export default function About({ mode }) {
   return (
-    <div style={{ color: mode === "dark" ? "white" : "#1b4b64" }}>
+    <div
+      style={{
+        color: mode === "dark" ? "white" : "#1b4b64",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       The goal of this project is to create a web-based application that allows
       users to practice typing. It provides various features like changing text
       case, clearing text, starting and stopping a timer, and providing text
       statistics.
+      <div>
+        When the timer is stoped, it shows the total time taken and the accuracy
+        of the typing.
+      </div>
+      <div>Click on Clear Text button to reset the timer and accuracy.</div>
     </div>
   );
 }
