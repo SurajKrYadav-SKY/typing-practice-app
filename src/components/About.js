3a45194cc08049acdd0d841883424d8112f662dd
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2024-03-10 13:58:38 +0530

    about component modified

--- src/components/About.js
+++ src/components/About.js
@@ -6,19 +6,39 @@ export default function About({ mode }) {
       style={{
         color: mode === "dark" ? "white" : "#1b4b64",
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
+        gap: "20px",
       }}
     >
-      The goal of this project is to create a web-based application that allows
-      users to practice typing. It provides various features like changing text
-      case, clearing text, starting and stopping a timer, and providing text
-      statistics.
-      <div>
+      <div style={{ fontSize: "20px" }}>
+        The goal of this project is to create a web-based application that
+        allows users to practice typing. It provides various features like
+        changing text case, clearing text, starting and stopping a timer, and
+        providing text statistics.
+      </div>
+      <div style={{ fontSize: "16x" }}>
         When the timer is stoped, it shows the total time taken and the accuracy
         of the typing.
       </div>
-      <div>Click on Clear Text button to reset the timer and accuracy.</div>
+      <div style={{ fontSize: "16px" }}>
+        Click on Clear Text button to reset the timer and accuracy.
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <img
+          src="./icons/gestures.png"
+          alt=""
+          style={{ width: "40px", height: "40px" }}
+        />
+      </div>
+      <div style={{ fontSize: "16px", fontWeight: "bold" }}>Thankyou!</div>
     </div>
   );
 }
